473477ff4922aaa1b92bb71518feda9a23bfba4e
Nicholas Dwork <ndwork@Nicholass-Mac-Pro.local>
2015-04-10 09:08:06 -0700

    Fixed two bugs in bounded optical flow code:   1) Forgetting the w term in Ay   2) Needed a factor of 2 in applyM Added orientation ability to makeAlignmentVideo Seems to be working; give very good results.

--- makeAlignmentVideo.m
+++ makeAlignmentVideo.m
@@ -7,9 +7,11 @@ function makeAlignmentVideo
   %datacase = 1;
   datacase = 9;
   %datacase = 11;
-  
+
+  orientation = 1;   % 0 - 3x2, 1 - 2x3
+
   outDir = ['alignImgs_',num2str(datacase)];
-  
+
   [x,eta,rho,offset] = loadData( datacase );
   x = abs(x);
   [nRows,nCols,nPages,nData] = size(x);
@@ -18,10 +20,11 @@ function makeAlignmentVideo
   midSlice = ceil(nPages/2);
   nData = min([offset+nData,maxNData,nData]);
 
+  bound = 1.2;
 
   mkdir( outDir );
   data1 = squeeze( x(:,:,:,offset+1) );
-  parfor i=1:nData-offset 
+  parfor i=1:nData-offset
     disp(['Working on image ', num2str(i)]);
     data2 = squeeze( x(:,:,:,offset+i) );
 
@@ -29,34 +32,55 @@ function makeAlignmentVideo
     scales = data1(maskedIndxs) ./ data2(maskedIndxs);
     data2scaled = data2 .* median( scales );
 
-    [du,dv,dw] = opticalFlow3D( data1, data2scaled, eta, rho );
+    %[du,dv,dw] = opticalFlow3D( data1, data2scaled, eta, rho );
+    [du,dv,dw] = boundedOpticalFlow3D( data1, data2, eta, rho, bound );
     interp2 = ofInterp3D( data2, du, dv, dw );
 
     axial2 = data2(:,:,midSlice);
     axialInterp = interp2(:,:,midSlice);
-    axialImg2Save = [ axial2 axialInterp ];
-    [~, nColsAx] = size(axialImg2Save);
-
-    sag2 = rot90( squeeze( data2(:,midCol,:) ) );
-    sagInterp = rot90( squeeze( interp2(:,midCol,:) ) );
-    sagImg2Save = [ sag2 sagInterp ];
-    [~, nColsSag] = size(sagImg2Save);
-    sagImg2Save = imresize( sagImg2Save, nColsAx/nColsSag );
+    if orientation==0
+      axialImg2Save = [ axial2 axialInterp ];
+    else
+      axialImg2Save = [ axial2; axialInterp; ];
+    end
+    [nRowsAx, nColsAx] = size(axialImg2Save);
 
     cor2 = rot90( squeeze( data2(midRow,:,:) ) );
     corInterp = rot90( squeeze( interp2(midRow,:,:) ) );
-    corImg2Save = [ cor2 corInterp ];
-    [~, nColsCor] = size(corImg2Save);
-    corImg2Save = imresize( corImg2Save, nColsAx/nColsCor );
+    if orientation==0    
+      corImg2Save = [ cor2 corInterp ];
+      [~, nColsCor] = size(corImg2Save);
+      corImg2Save = imresize( corImg2Save, nColsAx/nColsCor );
+    else
+      corImg2Save = [ cor2; corInterp; ];
+      [nRowsCor, ~] = size(corImg2Save);
+      corImg2Save = imresize( corImg2Save, nRowsAx/nRowsCor );
+    end
+    
+    sag2 = rot90( squeeze( data2(:,midCol,:) ) );
+    sagInterp = rot90( squeeze( interp2(:,midCol,:) ) );
+    if orientation==0
+      sagImg2Save = [ sag2 sagInterp ];
+      [~, nColsSag] = size(sagImg2Save);
+      sagImg2Save = imresize( sagImg2Save, nColsAx/nColsSag );
+    else
+      sagImg2Save = [ sag2; sagInterp; ];
+      [nRowsSag, ~] = size(sagImg2Save);
+      sagImg2Save = imresize( sagImg2Save, nRowsAx/nRowsSag );
+    end
 
-    img2Save = [ axialImg2Save; sagImg2Save; corImg2Save ];
+    if orientation==0
+      img2Save = [ axialImg2Save; corImg2Save; sagImg2Save; ];
+    else
+      img2Save = [ axialImg2Save corImg2Save sagImg2Save ];
+    end
     img2Save = imresize( img2Save, 3 );
 
-    % Add text onto image
-    textImg = 1 - text2im( ['Frame ', num2str(i,'%3.3i')] );
-    sImText = size(textImg);
-    img2Save(end-sImText(1)-9:end-10,20:19+sImText(2)) = ...
-      textImg*max(img2Save(:));
+    % % Add text onto image
+    %textImg = 1 - text2im( ['Frame ', num2str(i,'%3.3i')] );
+    %sImText = size(textImg);
+    %img2Save(end-sImText(1)-9:end-10,20:19+sImText(2)) = ...
+    %  textImg*max(img2Save(:));
 
     %imshow( img2Save, [] );
     imwrite( img2Save, ...
