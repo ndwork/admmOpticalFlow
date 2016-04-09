db464c2c8bbe7480a04012b04c10650350d525e6
ndwork <ndwork@ee-mbpro-ndwork.stanford.edu>
2016-04-09 13:25:42 -0700

    Minor changes.

--- runOpticalFlow2D.m
+++ runOpticalFlow2D.m
@@ -2,30 +2,32 @@
 
 function runOpticalFlow2D
 
-  %img1 = double( imread( '../data/basketball/frame13.png' ) ) / 255;
+  img1 = double( imread( '../../data/basketball/frame13.png' ) ) / 255;
   %img2 = circshift( img1, [2 2] );
-  %img2 = double( imread( '../data/basketball/frame14.png' ) ) / 255;
-  %img1 = double( imread( '../data/Yosemite/frame10.png' ) ) / 255;
-  %img2 = double( imread( '../data/Yosemite/frame11.png' ) ) / 255;
-  img1 = double( imread( '../Dumptruck/frame10.png' ) ) / 255;
-  img2 = double( imread( '../Dumptruck/frame11.png' ) ) / 255;
+  img2 = double( imread( '../../data/basketball/frame14.png' ) ) / 255;
+  %img1 = double( imread( '../../data/Yosemite/frame10.png' ) ) / 255;
+  %img2 = double( imread( '../../data/Yosemite/frame11.png' ) ) / 255;
+  %img1 = double( imread( '../../data/Dumptruck/frame10.png' ) ) / 255;
+  %img2 = double( imread( '../../data/Dumptruck/frame11.png' ) ) / 255;
 
   tic;
+  eta = 1d-3;
   profile on
-  [du,dv] = opticalFlow2D( img1, img2 );
+  [du,dv] = opticalFlow2D( img1, img2, eta );
   profile off
   timeTaken = toc;
   profile viewer
 
-  interped2 = ofInterp( img2, du, dv );
+  interped2 = ofInterp2D( img2, du, dv );
   close all;
   figure, imshow( img1, [] );
   title('img1', 'FontSize', 20 );
-  figure, imshow( interped2, [min(img1(:)) max(img1(:))] );
-  title('interped2', 'FontSize', 20 );
   figure, imshow( img2, [] );
   title('img2', 'FontSize', 20 );
+  figure, imshow( interped2, [min(img1(:)) max(img1(:))] );
+  title('interped2', 'FontSize', 20 );
 
+´
   disp([ 'Time taken (s): ', num2str(timeTaken) ]);
 end
 
